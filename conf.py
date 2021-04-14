49c601484b3ff31eb48b64e7d955200a3a434dd4
Anton Plagemann <54081139+antonplagemann@users.noreply.github.com>
2021-04-14 08:03:30 +0200

    Updated conf.py example

---
+++ conf.py
@@ -0,0 +1,11 @@
+# Your Monica api token
+TOKEN = 'YOUR_TOKEN_HERE'
+# Your Monica base url
+BASE_URL = 'https://app.monicahq.com/api'
+# Create reminders for birthdays and deceased days?
+CREATE_REMINDERS = True
+# Delete Monica contact if the corresponding Google contact has been deleted?
+DELETE_ON_SYNC = True
+# Do a street reversal in address sync if the first character is a number.
+# E.g. from '13 Auenweg' to 'Auenweg 13'
+STREET_REVERSAL = False
